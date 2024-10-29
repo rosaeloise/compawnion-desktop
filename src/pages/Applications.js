2124a45e337aa31e07290afcc080d90f540f14f1
Ely Rose Bosangit <bosangitelyrose05@gmail.com>
2024-10-30 01:41:37 +0800

    fetch sidebar contents

--- src/pages/Applications.js
+++ src/pages/Applications.js
@@ -11,37 +11,38 @@ class Applications extends React.Component {
 			loading: true,
 			user: {
 				avatar: '',
-				name: {
-					first: '',
-					last: ''
-				},
+				name: '',
 				role: '',
 				username: ''
-			}
+			},
 		};
 	};
 	componentDidMount() {
-		setTimeout(() => {
-			this.setState({
-				loading: false,
-				user: {
-					avatar: 'https://scontent.fmnl8-1.fna.fbcdn.net/v/t1.6435-9/98343921_3964488750260462_3832610965619212288_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=dd6889&_nc_eui2=AeGm5O07sV4g2mWQJmqJ1-O-377aGaUqoBLfvtoZpSqgEsD_5TyQ1z8RgL1MZRyaVA-j34PtB1K_aLxqUbhLceQH&_nc_ohc=ki5IaXRBJwcQ7kNvgEkKQkZ&_nc_ht=scontent.fmnl8-1.fna&oh=00_AYAb4j1j4QVfh2vd-O-S1_AsIqj5A9mtV2U15ySydQl4TA&oe=6724C727',
-					name: {
-						first: 'Gumoan',
-						last: 'Magsaysay'
-					},
-					role: 'Administrator',
-					username: 'gumoan'
-				}
+		fetch('http://localhost:3000/admins/me', {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+				'Authorization': `Bearer ${localStorage.getItem('token')}`
+			}
+		})
+			.then(res => res.json())
+			.then(res => {
+				this.setState({
+					user: {
+						avatar: res.aStaffInfo.Picture,
+						name: res.aStaffInfo.Name,
+						role: res.aStaffInfo.Branches,
+						username: res.aStaffInfo.Username
+					}
+				});
 			});
-		}, 1000);
 	};
 	render() {
 		return (
 			<>
 				<Sidebar
 					avatar={this.state.user.avatar}
-					name={this.state.user.name.first + ' ' + this.state.user.name.last}
+					name={this.state.user.name}
 					role={this.state.user.role}
 
 					active='applications'
