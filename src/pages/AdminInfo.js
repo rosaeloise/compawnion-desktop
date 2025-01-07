055fba47bcbc90e69b8095237ee00f57b33e15ad
Daniel John Baynosa <firedantsm@gmail.com>
2025-01-07 15:32:06 +0800

    feat: implement admin information update with validation and SweetAlert2 notifications

--- src/pages/AdminInfo.js
+++ src/pages/AdminInfo.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
+
+const MySwal = withReactContent(Swal);
+
 import Sidebar from '../components/Sidebar';
 import Button from '../components/Button';
 import Popup from '../components/Popup';
@@ -30,8 +35,24 @@ class AdminInfo extends React.Component {
 				Mobilenumber: '',
 				Branches: ''
 			},
+			admin: {
+				id: '',
+				LastLogout: null,
+				LastLogin: '',
+				token: '',
+				aStaffInfo: {
+					Name: '',
+					Picture: '',
+					Username: '',
+					Password: '',
+					Email: '',
+					Mobilenumber: '',
+					Branches: ''
+				}
+			},
 			popupContent: <></>,
-			petID: 0
+			saveDisabled: false,
+			id: 0
 		};
 	};
 	componentDidMount() {
@@ -55,7 +76,8 @@ class AdminInfo extends React.Component {
 							email: res.aStaffInfo.Email,
 							phone: res.aStaffInfo.Mobilenumber
 						},
-						aStaffInfo: res.aStaffInfo
+						aStaffInfo: res.aStaffInfo,
+						admin: res
 					});
 				} catch (error) {
 					localStorage.removeItem('token');
@@ -64,7 +86,139 @@ class AdminInfo extends React.Component {
 			});
 	};
 
-	saveAdminInfo = async (e) => { };
+	saveAdminInfo = async (e) => {
+		this.saveDisabled = true;
+		const admin = this.state.admin;
+		const AdminId = location.hash.split('/').pop();
+		const Name = this.state.admin.aStaffInfo.Name;
+		const Email = this.state.admin.aStaffInfo.Email;
+		const Mobilenumber = this.state.admin.aStaffInfo.Mobilenumber;
+		console.log(admin);
+
+
+		admin.Name = Name;
+		admin.Email = Email;
+		admin.Mobilenumber = Mobilenumber;
+		delete admin.id;
+
+		if (!Name || !Email || !Mobilenumber) {
+			MySwal.fire({
+				title: <h4>Error</h4>,
+				html: <p>Please fill in all fields.</p>,
+				width: '60rem',
+				icon: 'error',
+				iconColor: 'var(--primary-color)',
+				confirmButtonText: 'Ok',
+				confirmButtonColor: 'var(--primary-color)'
+			});
+			this.saveDisabled = false;
+			return;
+		}
+
+		const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+		if (!emailRegex.test(Email)) {
+			MySwal.fire({
+				title: <h4>Error</h4>,
+				html: <>
+					<p>Invalid email format.</p>
+					<p>Please enter a valid email address.</p>
+				</>,
+				width: '60rem',
+				icon: 'error',
+				iconColor: 'var(--primary-color)',
+				confirmButtonText: 'Ok',
+				confirmButtonColor: 'var(--primary-color)'
+			});
+			this.saveDisabled = false;
+			return;
+		}
+
+		if (Mobilenumber.length !== 11) {
+			MySwal.fire({
+				title: <h4>Error</h4>,
+				html: <p>Phone number must be exactly 11 digits.</p>,
+				width: '60rem',
+				icon: 'error',
+				iconColor: 'var(--primary-color)',
+				confirmButtonText: 'Ok',
+				confirmButtonColor: 'var(--primary-color)'
+			});
+			this.saveDisabled = false;
+			return;
+		}
+
+		const PhoneRegex = /^[0-9]*$/;
+		if (!PhoneRegex.test(Mobilenumber)) {
+			MySwal.fire({
+				title: <h4>Error</h4>,
+				html: <p>Phone number must contain only numbers.</p>,
+				width: '60rem',
+				icon: 'error',
+				iconColor: 'var(--primary-color)',
+				confirmButtonText: 'Ok',
+				confirmButtonColor: 'var(--primary-color)'
+			});
+			this.saveDisabled = false;
+			return;
+		}
+
+		const send = async () => {
+			fetch(`https://compawnion-backend.onrender.com/admins/${this.state.user.id}`, {
+				method: 'PUT',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify(admin)
+			})
+				.then(async res => {
+					if (!res.ok) {
+						const message = await res.json();
+						throw new Error(message.message);
+					}
+					return res.json();
+				})
+				.then(() => {
+					MySwal.fire({
+						title: <h4>Success</h4>,
+						html: <p>Admin updated successfully.</p>,
+						width: '60rem',
+						icon: 'success',
+						confirmButtonText: 'Ok',
+						confirmButtonColor: 'var(--primary-color)'
+					}).then(() => {
+						window.location.reload();
+					});
+					this.saveDisabled = false;
+				})
+				.catch(err => {
+					MySwal.fire({
+						title: <h4>Error</h4>,
+						html: <>
+							<p>Failed to update admin.</p>
+							<p>{err.message}</p>
+						</>,
+						width: '60rem',
+						icon: 'error',
+						iconColor: 'var(--primary-color)',
+						confirmButtonText: 'Ok',
+						confirmButtonColor: 'var(--primary-color)'
+					});
+					this.saveDisabled = false;
+				});
+		};
+
+		const imageInput = document.getElementById('imageInput').files[0];
+		if (imageInput instanceof Blob) {
+			const reader = new FileReader();
+			reader.readAsDataURL(imageInput);
+			reader.onload = async () => {
+				admin.Picture = reader.result;
+				await send();
+			};
+		} else {
+			send();
+		}
+	};
 
 	render() {
 		return (
@@ -89,6 +243,7 @@ class AdminInfo extends React.Component {
 								title='Save'
 								id='save'
 								onClick={this.saveAdminInfo}
+								disabled={this.state.saveDisabled}
 							/>
 						</div>
 					</header>
@@ -116,7 +271,7 @@ class AdminInfo extends React.Component {
 								}}
 
 								style={{
-									backgroundImage: this.state.aStaffInfo.Picture ? `url(${this.state.aStaffInfo.Picture})` : ''
+									backgroundImage: this.state.admin.aStaffInfo.Picture ? `url(${this.state.admin.aStaffInfo.Picture})` : ''
 								}}
 							/>
 							<Button
@@ -135,60 +290,93 @@ class AdminInfo extends React.Component {
 							<FormInput
 								label='Name'
 								type='text'
-								id='name'
-								name='name'
+								id='Name'
+								name='Name'
 								placeholder='Name'
-								value={this.state.aStaffInfo.Name}
-								defaultValue={this.state.aStaffInfo.Name}
+								value={this.state.admin.aStaffInfo.Name}
+								defaultValue={this.state.admin.aStaffInfo.Name}
+								onChange={(e) => {
+									this.setState({
+										admin: {
+											...this.state.admin,
+											aStaffInfo: {
+												...this.state.admin.aStaffInfo,
+												Name: e.target.value
+											}
+										}
+									});
+								}}
 							/>
 							<FormInput
 								label='Type'
 								type='text'
-								id='type'
+								id='Username'
 								name='type'
 								placeholder='Username'
 								disabled={true}
-								value={this.state.aStaffInfo.Username}
-								defaultValue={this.state.aStaffInfo.Username}
+								value={this.state.admin.aStaffInfo.Username}
+								defaultValue={this.state.admin.aStaffInfo.Username}
 							/>
 							<FormInput
 								label='Branch'
 								type='text'
-								id='ageYear'
-								name='ageYear'
+								id='Branches'
+								name='Branches'
 								placeholder='Branch'
 								disabled={true}
-								value={this.state.aStaffInfo.Branches}
-								defaultValue={this.state.aStaffInfo.Branches}
+								value={this.state.admin.aStaffInfo.Branches}
+								defaultValue={this.state.admin.aStaffInfo.Branches}
 							/>
 						</div>
 						<div>
 							<FormInput
 								label='Admin'
 								type='text'
-								id='petID'
-								name='petID'
+								id='Admin'
+								name='Admin'
 								disabled={true}
-								value={this.state.petID}
-								defaultValue={this.state.petID}
+								value={this.state.user.id}
+								defaultValue={this.state.user.id}
 							/>
 							<FormInput
 								label='Email'
 								type='test'
-								id='breed'
-								name='breed'
+								id='Email'
+								name='Email'
 								placeholder='Email'
-								value={this.state.aStaffInfo.Email}
-								defaultValue={this.state.aStaffInfo.Email}
+								value={this.state.admin.aStaffInfo.Email}
+								defaultValue={this.state.admin.aStaffInfo.Email}
+								onChange={(e) => {
+									this.setState({
+										admin: {
+											...this.state.admin,
+											aStaffInfo: {
+												...this.state.admin.aStaffInfo,
+												Email: e.target.value
+											}
+										}
+									});
+								}}
 							/>
 							<FormInput
 								label='Phone Number'
 								type='number'
-								id='gender'
-								name='gender'
+								id='Mobilenumber'
+								name='Mobilenumber'
 								placeholder='Phone Number'
-								value={this.state.aStaffInfo.Mobilenumber}
-								defaultValue={this.state.aStaffInfo.Mobilenumber}
+								value={this.state.admin.aStaffInfo.Mobilenumber}
+								defaultValue={this.state.admin.aStaffInfo.Mobilenumber}
+								onChange={(e) => {
+									this.setState({
+										admin: {
+											...this.state.admin,
+											aStaffInfo: {
+												...this.state.admin.aStaffInfo,
+												Mobilenumber: e.target.value
+											}
+										}
+									});
+								}}
 							/>
 						</div>
 					</section>
